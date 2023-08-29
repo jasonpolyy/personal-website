c0580f4bbd0c542f073bc2bb5a9b00ba609a71cf
jamuupolyy <42693126+jamuupolyy@users.noreply.github.com>
2023-08-29 20:47:53 +1000

    downsized photos

--- config/photos.js
+++ config/photos.js
@@ -1,10 +1,10 @@
-import React, { Fragment } from 'react';
-import photo1 from './photos/photo1.png';
-import photo2 from './photos/photo2.png';
-import photo3 from './photos/photo3.png';
-import photo4 from './photos/photo4.png';
-import photo5 from './photos/photo5.png';
-import photo6 from './photos/photo6.png';
+// import React, { Fragment } from 'react';
+import photo1 from './photos/photo1.jpg';
+import photo2 from './photos/photo2.jpg';
+import photo3 from './photos/photo3.jpg';
+import photo4 from './photos/photo4.jpg';
+import photo5 from './photos/photo5.jpg';
+import photo6 from './photos/photo6.jpg';
 
 export const photo_src = {
 
